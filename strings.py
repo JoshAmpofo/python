9bbe03c000eb01ee9b934ca7d967e2042fa5c160
JoshAmpofo <joshampofo92@gmail.com>
2022-12-26 12:29:14 +0000

    string methods

---
+++ strings.py
@@ -0,0 +1,20 @@
+# working with strings
+#name = "ada lovelace"
+#print(name.title())
+#print(name.upper())
+#print(name.lower())
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = f"{first_name} {last_name}"
+#message = f"Hello, {full_name.title()}!"
+#print(message)
+
+# Using whitespaces and tabs to format print output
+# print("\tPython")
+#print("Languages:\n\tPython\n\tC\n\tJavascript")
+fav_lang = "python '"
+print(fav_lang)
+fav_lang = " python  "
+print(fav_lang.strip()) # removing whitespaces on both sides
+# .rstrip(): remove whitespace from right side
+# .lstrip(): remove whitespace from left side
