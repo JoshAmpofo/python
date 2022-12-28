6d3ac2a7fdca2b83c08ad3a073c002f3c31e3914
JoshAmpofo <joshampofo92@gmail.com>
2022-12-28 19:50:36 +0000

    Working with numbers

---
+++ numbers.py
@@ -0,0 +1,22 @@
+# Exercise
+# Number Eight: Write addition, subtraction, multiplication,
+# and division operations that each result in the number 8
+# Be sure to enclose your operations in print statements
+# to see the results. You should create four lines that 
+# look like this:
+    # print(5 + 3)
+# Your output should simply be four lines with the number
+# 8 appearing once on each line
+ 
+add_num = print(5 + 3)
+sub_num = print(12 - 4)
+mult_num = print(4 * 2)
+div_num = print(16 / 2)
+
+# Favorite number: Store your favorite number in a variable.
+# Then, using that variable, create a message that reveals
+# your favorite number. Print that message
+
+fav_num = 5792
+message = f"My favorite number is {fav_num:,}"
+print(message) 
