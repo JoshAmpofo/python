7dcd7cd255e3a1a3f84b4c8ae497d0b5d9d7d944
JoshAmpofo <joshampofo92@gmail.com>
2022-12-29 22:39:27 +0000

    update on numbers.py

---
+++ bases.py
@@ -0,0 +1,15 @@
+# Base conversions
+value = 65
+base_two = print("65 in binary: {}".format(bin(value)))
+base_eight = print("65 in octal: {}".format(oct(value)))
+base_sixteen = print("65 in hex: {}".format(hex(value)))
+
+# convert to character function
+chr() # converts an integer to its single-character string equivalent
+print(chr(65))
+
+# convert from character to digit
+print(ord('A')) # I think C does it better and much simpler
+# print(int(True)) # evaluates to 1
+# print(int(False)) # evaluates to 0
+
