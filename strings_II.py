b846b9fbc4bec83dee685c45547c31a62e4c40ca
JoshAmpofo <joshampofo92@gmail.com>
2023-01-01 02:41:59 +0000

    Update on strings

---
+++ strings_II.py
@@ -0,0 +1,38 @@
+# poem = """There was a Young Lady of Norway,
+# Who casually sat in a doorway;
+# When the door squeezed her flat
+# She exclaimed, 'What of that?'
+# This courageous Young Lady of Norway."""
+# print(poem)
+
+# poem_2 = '''I do not like thee, Doctor Fell.
+#     The reason why, I cannot tell.
+#     But this I know, and know full well:
+#     I do not like thee, Doctor Fell.
+# '''
+# print(poem_2)
+
+# palindrome = 'A man, \nA plan, \nA canal: \nPanama.'
+# print(palindrome)
+
+# the r (raw string) formatting negates any type of escape
+# used in a string.
+# info = r'Type a \n to get a new line in a normal string'
+# print(info)
+
+# poem_3 = r'''Boys and girls, come out to play.
+# The moon doth shine as bright as day.'''
+# print(poem_3)
+
+# msg = "My word! ""A gentleman caller"
+# print(msg)
+
+# duplicate with *
+# start = 'Na ' * 4 + '!' + '\n'
+# middle = 'Hey ' * 3 + '!' + '\n'
+# end = 'Goodbye.'
+# print(start + start + middle + end)
+
+# Replacing strings
+name = "Koshe"
+print(name.replace('e', 'a'))
