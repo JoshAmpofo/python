a4bf7899f43a78121ccb2adc2d125e6be92a301b
JoshAmpofo <joshampofo92@gmail.com>
2023-01-05 21:10:09 +0000

    Working with loops

---
+++ loops.py
@@ -0,0 +1,24 @@
+#***Repeat with while***
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+
+#***Cancelling with break***
+# while True:
+#     text = input("String to capitalize [type q to quit]: ")
+#     if text == 'q':
+#         break
+#     print(text.capitalize())
+
+#***Skip ahead with continue***
+while True:
+    value = input("Integer, please [q to quit]: ")
+    if value == 'q':
+        break
+    number = int(value)
+    if number % 2 == 0: # even number
+        print("Number is even, not what we want!")
+        continue
+    print(f"{number} squared is {number**2}")
+    
