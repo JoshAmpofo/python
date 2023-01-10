5657c21e144fa18cb75ef4fe593273851ad9e09b
JoshAmpofo <joshampofo92@gmail.com>
2023-01-10 06:58:52 +0000

    working with DSA: Lists and Tuples

--- tuples_list.py
+++ tuples_list.py
@@ -90,5 +90,72 @@
 # print(talk_like_a_pirate_day.split('/'))
 
 # splitting when delimiter is '//'
-splitme = 'a/b//c/d///e'
-print(splitme.split('//'))
+# splitme = 'a/b//c/d///e'
+# print(splitme.split('//'))
+
+# SLICING
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# print(marxes[0:2])
+
+# print(marxes[::2])
+# print(marxes[::-2])  # start from the back and move two steps to the right
+# print(marxes[::-1])  # start from the back and move 1 step to the left - this is reversal
+# These slices do not modify the lis in place. To modify in place:
+# marxes.reverse()  # function to use to reverse in place
+# print(marxes)  # reversed list, changes value but returns None
+
+# print(marxes[4:])
+# print(marxes[-6:])
+
+# LIST METHODS
+# marxes.append('Zeppo')
+# print(marxes)
+
+# marxes.insert(2, 'Gummo')
+# print(marxes)
+# marxes.insert(10, "Zeppo")
+# print(marxes)
+
+# List can also be duplicated using *
+# print(["word"] * 3)
+
+# Combine lists using extend() or +
+# marxes = ["Groucho", "Chico", "Harpo", "Zeppo"] 
+# others = ["Gummo", "Karl"]
+# marxes.extend(others)
+# marxes += others  # Alternatively
+# print(marxes)
+
+# You can change the item in a list by using its offset
+# marxes[2] = 'Wanda'
+# print(marxes)
+
+# Changing the values in a list using slicing
+# numbers = [1, 2, 3, 4]
+# numbers[1:3] = [8, 9]
+# numbers[1:3] = [7, 8, 9]  # diff length can be inserted into a list
+# numbers[1:3] = []
+# numbers[1:3] = (98, 99, 100)
+# numbers[1:3] = "wat?"
+# print(numbers)
+
+# Deleting list items wih del
+# marxes = ["Groucho", "Chico", "Harpo", "Gummo", "Karl"]
+# print(marxes[-1])
+# del marxes[-1]
+# print(marxes)
+
+# rep_list = ["rep", "rep_2", "rep", "rep_3"]
+# rep_list.remove("rep")
+# print(rep_list)
+
+# get and delete an item using pop()
+# print(rep_list.pop(0))
+# print(rep_list.pop())
+# print(rep_list)
+
+# Remove all items using clear()
+work_quotes = ["working hard?", "Quick question!", "Number one priorities!"]
+# print(work_quotes)
+work_quotes.clear()
+print(work_quotes)
