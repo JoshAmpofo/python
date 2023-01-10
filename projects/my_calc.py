e25ea843d3da99a79475d0c1022b9ba471759ac0
JoshAmpofo <joshampofo92@gmail.com>
2023-01-10 07:38:27 +0000

    Complete first draft

--- projects/my_calc.py
+++ projects/my_calc.py
@@ -11,18 +11,30 @@ def mul(a, b):
 def div(a, b):
     return a / b
 
-# create dictionary to store operands
-ops = {"add": "+",
-        "sub": "-",
-        "mul": "*",
-        "div": "/"
-        }
+# create list to store operands
+ops = ['+', '-', '*', '/']
 
 # Ask user for input
 print("******* Welcome to the Awesome Calculator *******")
 print("What operation do you want to perform: ")
 print("Operators are: \n")
 print("""addition: 1\nsubtraction: 2\nmultiplication: 3\ndivision: 4\n""")
-print(input("Select option[choose number]: "))
-#for i, (key, value) in enumerate(ops.items()):
- #   print(f"Index {i}: {key}, {value}")
+user_input_option = (input("Select option[choose number]: "))
+
+# Ask user to enter two numbers
+a = float(input("\nEnter first number: "))
+b = float(input("Enter second number: "))
+
+# Display chosen operation option
+if user_input_option == "1":
+    print(f"You have chosen {user_input_option}: add\n")
+    print(f"Results: {a} {ops[0]} {b} = {add(a, b)}")
+elif user_input_option == "2":
+     print(f"You have chosen {user_input_option}: subtract\n")
+     print(f"Results: {a} {ops[1]} {b} = {sub(a, b)}")
+elif user_input_option == "3":
+     print(f"You have chosen {user_input_option}: multiply\n")
+     print(f"Results: {a} {ops[2]} {b} = {mul(a, b)}")     
+elif user_input_option == "4":
+     print(f"You have chosen {user_input_option}: divide\n")
+     print(f"Results: {a} {ops[3]} {b} = {div(a, b)}")
