9d8b2e73662b24ba7db38f8b24382470a29ba60b
JoshAmpofo <joshampofo92@gmail.com>
2023-01-16 20:17:06 +0000

    more error handling

--- webscraping/scrapetest.py
+++ webscraping/scrapetest.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
 
+# from urllib.request import urlopen
+# from urllib.error import HTTPError
+# from urllib.error import URLError
+# from bs4 import BeautifulSoup
+
+# try:
+#     html = urlopen('https://pythonscraping.com/pages/page1.html')
+# except HTTPError as err:
+#     print(err)
+# except URLError as err:
+#     print("Server could not be found!")
+# else:
+#     bs = BeautifulSoup(html.read(), 'html.parser')
+#     print(bs.h1)
+
+########################################################
+# Handling Attribute Errors 
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 
-try:
-    html = urlopen('https://pythonscraping.com/pages/page1.html')
-except HTTPError as err:
-    print(err)
-except URLError as err:
-    print("Server could not be found!")
+def getTitle(url):
+    try:
+        html = urlopen(url)  # read url
+    except HTTPError as err:  # if url cannot be found
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1  # retriee tag
+    except AttributeError as err:  #  if tag doesn't exist
+        return None 
+    return title
+# get target URL
+title = getTitle('https://www.pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
 else:
-    bs = BeautifulSoup(html.read(), 'html.parser')
-    print(bs.h1)
+    print(title)
