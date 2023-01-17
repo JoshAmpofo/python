c15ac43553990a664b58cb7f58065ae24838a5aa
JoshAmpofo <joshampofo92@gmail.com>
2023-01-17 21:34:36 +0000

    complete set methods

--- sets.py
+++ sets.py
@@ -1,104 +1,140 @@
 # Creating Sets
-# empty_set = set()
-# print(empty_set)
+empty_set = set()
+print(empty_set)
 
 # convert with set()
-# print(set('letters'))
+print(set('letters'))
 
 # CREATING SETS FROM LISTS
-# print(set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon']))
+print(set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon']))
 
 # CREATING FROM A TUPLE
-# print(set(('Ummagumma', 'Echoes', 'Atom Heart Mother')))
+print(set(('Ummagumma', 'Echoes', 'Atom Heart Mother')))
 
 # WORKING WITH DICTIONARIES
-# print(set({'name': 'Ampofo', 'age': 30, 'sex': 'male'}))
+print(set({'name': 'Ampofo', 'age': 30, 'sex': 'male'}))
 
 # FIND LENGTH OF A SET WITH len()
-# reindeer = set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon'])
-# print(len(reindeer))
+reindeer = set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon'])
+print(len(reindeer))
 
 # ADD AN ITEM WITH add()
-# s = set((1, 2, 3))
-# print(s)
-# s.add(4)
-# print(s)
+s = set((1, 2, 3))
+print(s)
+s.add(4)
+print(s)
 
 # DELETE AN ITEM WITH remove()
-# s.remove(3)
-# print(s)
+s.remove(3)
+print(s)
 
 # ITERATE WITH FOR AND IN
-# furniture = set(('sofa', 'ottoman', 'table'))
-# for piece in furniture:
-#     print(piece)
+furniture = set(('sofa', 'ottoman', 'table'))
+for piece in furniture:
+    print(piece)
 
 # TESTING FOR VALUE WITH in
-# drinks = {'martini': {'vodka', 'vermouth'},
-# 'black russian': {'vodka', 'kahlua'}, 'white russian':
-# {'cream', 'kahlua', 'vodka'}, 'manhattan': {'rye', 'vermouth', 'bitters'},
-# 'screwdriver': {'orange juice', 'vodka'}
-# }
+drinks = {'martini': {'vodka', 'vermouth'},
+'black russian': {'vodka', 'kahlua'}, 'white russian':
+{'cream', 'kahlua', 'vodka'}, 'manhattan': {'rye', 'vermouth', 'bitters'},
+'screwdriver': {'orange juice', 'vodka'}
+}
 
-# which dricnks contain vodka
-# for name, contents in drinks.items():
-    # if 'vodka' in contents:
-        # print(name)
+# which drinks contain vodka
+for name, contents in drinks.items():
+    if 'vodka' in contents:
+        print(name)
 
 # want vodka but are lactose intolerant and vermouth tastes like kerosene
-# for name, contents in drinks.items():
-#     if 'vodka' in contents and not ('vermouth' in contents or 'cream'in contents): 
-#         print(name)
+for name, contents in drinks.items():
+    if 'vodka' in contents and not ('vermouth' in contents or 'cream'in contents): 
+        print(name)
 
-# JUMPING TO LAMBDA, MAP, FILTER AND REDUCE()
-# lambda & map()
-# a = [1, 2, 3]
-# b = [17, 12, 11, 10]
-# c = [-1, -4, 5, 9]
+#USING COMBINATIONS and OPERATORS
+for name, contents in drinks.items():
+    if contents & {'vermouth', 'orange juice'}:  # check for drinks with vermouth and orange juice
+        print(name)
+        
+# want vodka but not vermouth and cream
+for name, contents in drinks.items():
+   if 'vodka' in contents and not contents & {'vermouth', 'cream'}:
+       print(name)
 
-# print(list(map(lambda x, y, z : 2.5*x + 2*y - z, a, b, c)))
+bruss = drinks['black russian']
+wruss = drinks['white russian']
 
-# filter
-# fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# odd_numbers = list(filter(lambda x: x % 2, fibonacci))
-# even_numbers = list(filter(lambda x: x % 2 == 0, fibonacci))
-# print(odd_numbers)
-# print(even_numbers)
+# intersection
+print(bruss & wruss)
+print(bruss.intersection(wruss))
+# union
+print(bruss | wruss)
+print(bruss.union(wruss))
+# difference
+print(bruss - wruss)
+print(bruss.difference(wruss))
+print(wruss - bruss)
+print(wruss.difference(bruss))
 
-# extended version
-# for number in fibonacci:
-#     if number % 2 == 1:
-#         print(number, end=" ")
+# other complex types of set operations
+a = {1, 2}
+b = {2, 3}
 
-# REDUCE
-# import functools
-# print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4]))
+# symmetric difference (mems in one set or the other but not both)
+print(a ^ b)
+print(a.symmetric_difference(b))
 
-#finding the max number in a list using reduce
-# from functools import reduce
-# f = lambda a, b : a if (a > b) else b
-# print(reduce(f, [47, 11, 42, 102, 13]))
+# subset (all mems in one set are also in the other)
+print(a <= b)
+print(a.issubset(b))
 
-# calculating the sum of numbers from 1 to 100
-# print(reduce(lambda x, y: x+y, range(1, 101)))
+# a set is always its own subset 
+print(a <= a)
+print(a.issubset(a))
 
-# my_list = [1, 2, 3, 1, 4, 2, 5]
-# result = 0
-# new_list = set(my_list)
-# for item in new_list:
-#     result +=  item
-# print(result)
+# proper subset (second set has all the mems of the 1st set)
+print(a < b)
 
-# set_1 = { "Python", "C", "Javascript" }
-# set_2 = { "Bash", "C", "Ruby", "Perl" }
-# print(set_1 & set_2)
+# superset (all second set mems are part of first set)
+print(a >= b)
+print(a.issuperset(b))
 
-# a_dict = {"language": "C", "number": 13, "track": "Low_level"}
-# count = 0
-# for key in a_dict.keys():
-#     count += 1
-# print(count)
+# Any set is a superset of itself
+print(a >= a)
+print(a.issuperset(a)) 
 
-# a_dict = {"language": "C", "number": [3, 1, 2], "track": "Low_level"}
-# for keys in sorted(a_dict.keys()):
-#     print("{}: {}".format(keys, a_dict[keys]))
+# Proper superset
+print(a > b)
+# a set cannot be a superset of itself
+print(a > a)
+
+# SET COMPREHENSION
+set_comp = {num for num in range(1, 10) if num % 3 == 1}
+print(set_comp)
+
+# FROZENSET
+print(frozenset([3, 2, 1]))
+print(frozenset(set([2, 1, 3])))
+
+# let's see if it is truly frozen
+fs = frozenset([1, 3, 2])
+fs.add(4)
+print(fs)
+fs.remove(2)
+print(fs)
+
+# MAKING BIGGER DATA STRUCTURES
+marxes = ['Groucho', 'Chico', 'Harpo']
+pythons = ['Chapman', 'Cleese', 'Gilliam', 'Jones', 'Palin']
+stooges = ['Moe', 'Curly', 'Larry']
+
+# make tuple of lists
+tuples_of_lists = marxes, pythons, stooges
+print(tuples_of_lists)
+
+# make list of lists
+list_of_lists = [marxes, pythons, stooges]
+print(list_of_lists)
+
+# make a dictionary of lists
+dict_of_lists = {'Marxes': marxes, 'Pythons': pythons, 'Stooges': stooges}
+print(dict_of_lists)
