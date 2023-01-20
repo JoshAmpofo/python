040c480a349c6b9bb44964db628771a64ef612eb
JoshAmpofo <joshampofo92@gmail.com>
2023-01-20 21:03:07 +0000

    rewrite print and return statements

--- projects/my_calc.py
+++ projects/my_calc.py
@@ -1,18 +1,18 @@
 # define functions
 def add(a, b):
-    print(a + b)
+    return a + b
         
 def sub(a, b):
-    print(a - b)
+    return a - b
     
 def mul(a, b):
-    print(a * b)
+    return a * b
     
 def div(a, b):
-    print(a / b)
+    return a / b
 
 def return_result(func, a, b):
-    func(a, b)
+   print(func(a, b))
 
 # Ask user to choose operand
 while True: # repeat action
