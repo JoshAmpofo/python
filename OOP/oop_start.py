07f0bd27b06577e5c194ce6cf02e46182bb484df
JoshAmpofo <joshampofo92@gmail.com>
2023-01-24 16:07:46 +0000

    update classes

--- OOP/oop_start.py
+++ OOP/oop_start.py
@@ -2,18 +2,18 @@
 class Person:
     pass
 
-# p = Person()  # object/instance/variable of the class
-# print(p)
+p = Person()  # object/instance/variable of the class
+print(p)
 
 # METHODS (colloquially known as functions)
-# class Person:
-    # def say_hi(self):
-        # print("Hello, how are you?")
+class Person:
+    def say_hi(self):
+        print("Hello, how are you?")
         
-# p = Person()
-# p.say_hi()  # calling the function
+p = Person()
+p.say_hi()  # calling the function
 # alternatively
-# Person().say_hi()  # also calling the function
+Person().say_hi()  # also calling the function
 
 # the __init__method()
 class Person:
@@ -27,3 +27,34 @@ class Person:
 p = Person('Josh')
 p.say_hi()
 Person("Josh").say_hi()
+
+class User:
+    id = 89
+    name = "no name"
+    __password = None
+    
+    def __init__(self, new_name=None):
+        self.is_new = True
+        if new_name is not None:
+            self.name = new_name
+            
+u = User()
+print(u.is_new)
+
+class Square:
+    """Create an empty class Square"""
+    pass
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
+
+class Square:
+    """Create an empty class Square"""
+    def __init__(self, size):
+        """Args:
+                size: private instance object useful for deterrming
+                some computational attribute of Square such as Area computation
+        """
+        self.__size = size
+          
