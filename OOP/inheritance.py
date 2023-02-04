0a6311cfb2fc3eb5871618d1097d3edcd2df8430
JoshAmpofo <joshampofo92@gmail.com>
2023-02-04 19:03:59 +0000

    Call parent method in child class

---
+++ OOP/inheritance.py
@@ -0,0 +1,62 @@
+#!/usr/bin/pythom3
+"""
+This module gives a sample code on how inheritance works
+"""
+
+class Car():
+    """define Car"""
+    def exclaim(self):
+        print("I am a car!")
+
+class Yugo(Car):
+    """child of parent Car"""
+    def exclaim(self):
+        print("I'm a Yugo! Much like a Car, but more Yugo-ish.")
+    def need_a_push(self):
+        print("A little help here")
+
+# create objects
+gimme_car = Car()
+gimme_yugo = Yugo()
+gimme_car.exclaim()
+gimme_car.need_a_push()  # error because Parent has no such method
+gimme_yugo.exclaim()  # will now inherit its own method (overwrite the parent)
+gimme_yugo.need_a_push()
+
+# __INIT__ METHOD
+class Person():
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
+#class MDPerson(Person):
+    def __init__(self, name):
+        self.name = "Doctor " + name
+        print(self.name)
+
+#class JDPerson(Person):
+    def __init__(self, name):
+        self.name = name + ", Esquire"
+        print(self.name)
+
+person = Person("Fudd")
+doctor = MDPerson("Fudd")
+lawyer = JDPerson("Fudd")
+print(person.name)
+print(doctor.name)
+print(lawyer.name)
+
+# CALLING PARENT METHOD from CHILD
+class Person():
+    """create a Person"""
+    def __init__(self, name):
+        self.name = name
+
+class EmailPerson(Person):
+    def __init__(self, name, email):
+        super().__init__(name)  # calls parent init method
+        self.email = email
+
+bob = EmailPerson("Bob Frapples", 'bob@frapples.com')
+print(bob.name)
+print(bob.email)
