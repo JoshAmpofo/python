11efc914b550d9345e72666b019d724e464bf3da
JoshAmpofo <joshampofo92@gmail.com>
2023-02-08 20:14:03 +0000

    Add print statements and refactor

--- projects/guess_game.py
+++ projects/guess_game.py
@@ -6,11 +6,19 @@ import random
 count = 5
 number = random.randint(1, 10)
 
+print("****** WELCOME TO GUESS-THE-NUMBER GAME ******\n")
+print("The number is between 1 and 10. You only have 5 tries.\nBEGIN!!!\n")
+
 while count > 0:
     guess = int(input("Enter a number: "))
-    if guess == 5:
-        print(f"That's the correct number!. You have {count} tries left")
+    if guess == number:
+        print(f"That's the correct number!!!. You won!,\
+ and You had {count} tries left!!!")
+        exit()
     else:
         count -= 1
-        print(f"Still trying. Number is now {guess}. {count} tries left")
-    
+        if count == 0:
+            print(f"Shoot, you didn't get it right and now GAME OVER!!!.\
+ The number was {number}")
+        else:
+            print(f"Wrong. You have {count} tries left")
