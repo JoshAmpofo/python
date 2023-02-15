7eecd7f694a122e028a736793ea6a384b1f5e99d
JoshAmpofo <joshampofo92@gmail.com>
2023-02-15 21:14:28 +0000

    Add new code line

--- OOP/get_stter.py
+++ OOP/get_stter.py
@@ -12,7 +12,25 @@ class Duck():
         print('inside the setter')
         self.hidden_name = input_name
 
-don = Duck("Donald")
-print(don.get_name())
-don.set_name("Donna")
-print(don.get_name())
+# don = Duck("Donald")
+# print(don.get_name())
+# don.set_name("Donna")
+# print(don.get_name())
+#####################################################################
+
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def diameter(self):
+        return 2 * self.radius
+
+    @diameter.setter
+    def diameter(self):
+        self.diameter = diameter
+
+c = Circle(7)
+#c.diameter = 20
+print(f"Radius is: {c.radius}")
+print(f"Diameter is: {c.diameter}")
