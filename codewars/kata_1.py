ed9cd73a43f00a8c6d73de8ceea6675653cce117
JoshAmpofo <joshampofo92@gmail.com>
2023-03-05 12:00:02 +0000

    Solve first kata challenge

---
+++ codewars/kata_1.py
@@ -0,0 +1,12 @@
+# Write a function that reverses words that have letters greater than 5
+
+def spinWords(sentence):
+    words = sentence.split()
+    for i in range(len(words)):
+        if len(words[i]) >= 5:
+            words[i] = words[i][::-1]
+    return ' '.join(words)
+
+print(spinWords( "Hey fellow warriors" ))
+print(spinWords( "This is a test" ))
+print(spinWords ("This is another test" ))
