9c4c2a425615ca80049a8f08653b605d0180107a
Joshua Ampofo Yentumi <78964254+JoshAmpofo@users.noreply.github.com>
2023-10-29 13:47:14 +0000

    Add Challenge day 1 solution

---
+++ 50_days_python/division_and_squareRoot_day_1.py
@@ -0,0 +1,27 @@
+"""
+Author: Joshua Ampofo Yentumi
+Purpose: Improving my python skills by solving a python challenge a day
+using the 50 Days of Python book by Benjamin Bennett Alexander
+
+Day One: Division and Square-root
+
+Project Description: This program takes a number as an argument and returns the 
+squareroot of the number is divisible by 5 or the remainder if not
+"""
+def divide_or_square(num):
+    """
+    Returns the squareroot or remainder if arg is divisble by 5 or not
+    Args:
+        num (int): number to check divisibility by 5
+    
+    Returns:
+        squareroot of number or remainder of number
+    """
+    from math import sqrt
+    if num % 5 == 0:
+        return sqrt(num)
+    else:
+        return num % 5
+
+# call function
+print(round(divide_or_square(120), 2))
