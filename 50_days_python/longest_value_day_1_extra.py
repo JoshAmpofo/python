2be9e785150be0fa890bd90a339c3bd70613b678
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2023-11-03 12:09:43 +0000

    Rename files

---
+++ 50_days_python/longest_value_day_1_extra.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.10
+
+"""
+Author: Joshua Ampofo Yentumi
+
+Day One Extra Challenge: Longest Value
+
+Project Description: This program takes a dictionary as an argument and returns the first 
+longest value in dictionary
+"""
+
+
+def longest_value(dict):
+    """
+    Args:
+        dictionary (dict): dictionary to check for longest value
+    Returns:
+        longest_value (str): longest value in dictionary
+    """
+    if not dict:
+        return None
+
+    long_value = max(dict.values(), key=len)
+    return long_value
+
+fruits = {'fruit': 'apple', 'color':'green'}
+print(longest_value(fruits))
