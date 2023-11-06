336f2cfcb35f12883405250e85b3968ec94b794b
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2023-11-06 18:36:27 +0000

    Add solution to challenge day 5

---
+++ 50_days_python/my_discount_day_5.py
@@ -0,0 +1,25 @@
+"""
+Author: Joshua Ampofo Yentumi
+    
+Day 5 Challenge: Discount
+    
+Description: Write a function that takes no argument but asks the user
+             to input the price and discount of the product.
+             Calculate the price of the product after the discount and return it.
+"""
+
+
+def my_discount():
+    """
+    Calculates the price of a product after discount
+    
+    Returns:
+        price of product after discount
+    """ 
+    price = float(input("Enter price of product: "))
+    discount = float(input("Enter discount: "))
+    price_after_discount = price - (price * discount / 100)
+    return price_after_discount
+
+
+print(my_discount())
