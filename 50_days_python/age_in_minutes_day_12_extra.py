18ad5b378b76a1fdb5e1a3d39fd771ea58f380be
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2023-11-17 20:36:55 +0000

    Calculate age in minutes

---
+++ 50_days_python/age_in_minutes_day_12_extra.py
@@ -0,0 +1,53 @@
+"""
+Author: Joshua Ampofo Yentumi
+
+Day 12 Extra Challenge: Your Age on Minutes
+
+Description: A function that tells a user how old they are in minutes.
+             The user inouts their year of birth and their age should be returned
+             in minutes by subtracting their year of birth from the current year.
+
+             The user can only input a 4-digit year of birth. Entering any number longer
+             or shorter than 4 digits should render the input invalid. User should be notified
+             that they must enter a four-digit number.
+             
+             If a user enters a year before 1900, the code should tell the user that input is invalid.
+             If user enters the year after the current year, code should tell the user to input a 
+             valid year.
+"""
+
+
+from datetime import datetime
+
+def age_in_minutes() -> int:
+    """
+    Calculates the age of a person in minutes based on their year of birth.
+    
+    Arg(s):
+        year_of_birth (int): year of birth of user
+
+    Returns:
+        age_minutes (int): age of user in minutes
+    """
+    year_of_birth = int(input('Enter your year of birth: '))
+    
+    if year_of_birth < 1000 or year_of_birth > 9999:
+        return "Invalid input. Please enter a four-digit year of birth."
+    
+    MINIMUM_YEAR = 1900
+    
+    # Check if the year_of_birth is before the minimum year or after the current year
+    current_year = datetime.now().year
+    if year_of_birth < MINIMUM_YEAR or year_of_birth > current_year:
+        return "Invalid year. Please enter a year between {} and the current year.".format(MINIMUM_YEAR)
+    
+    # Calculate the age in years
+    age_years = current_year - year_of_birth
+
+    # Convert the age in years to minutes (1 year = 525600 minutes)
+    age_minutes = age_years * 525600
+
+    return "You are {:,} minutes old".format(age_minutes)
+
+
+print(age_in_minutes())
