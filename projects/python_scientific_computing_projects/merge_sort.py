13e15ae0b10ec36dfc75a44e941d2eb81a7b0abb
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2024-01-15 12:54:43 +0000

    Improve comments

--- projects/python_scientific_computing_projects/merge_sort.py
+++ projects/python_scientific_computing_projects/merge_sort.py
@@ -13,26 +13,27 @@ def merge_sort(array):
     Return:
         list: sorted list
     """
-    # create recursion termination function
+    # create recursion termination function by checking if array is empty or has only one element
     if len(array) <= 1:
         return
     
-    # obtain two halves of array
+    # split array in half
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
     
-    # call sort function on both halves to sort each individually
+    # call merge_sort function recursively on each half
     merge_sort(left_part)
     merge_sort(right_part)
     
     # define sorting conditions
     
-    # set array indexes
+    # merge sorted halves
     left_array_index = 0 
     right_array_index = 0 
     sorted_index = 0
     
+    # compare elements at index of each half and add smaller element to final array
     while left_array_index < len(left_part) and right_array_index < len(right_part):
         if left_part[left_array_index] < right_part[right_array_index]:
             array[sorted_index] = left_part[left_array_index]
@@ -40,13 +41,17 @@ def merge_sort(array):
         else:
             array[sorted_index] = right_part[right_array_index]
             right_array_index += 1
+            
         sorted_index += 1
-    # sort left side     
+        
+        
+    # add remaining elements from left half    
     while left_array_index < len(left_part):
         array[sorted_index] = left_part[left_array_index]
         left_array_index += 1
         sorted_index += 1
-    # sort right side    
+    
+    # add remaining elements from right half
     while right_array_index < len(right_part):
         array[sorted_index] = right_part[right_array_index]
         right_array_index += 1
