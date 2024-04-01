c99d1ee3dd9ef10895b61a02909fb7e153ac7d13
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2024-04-01 14:33:34 +0000

    calculates average calories

---
+++ 50_days_python/average_calories_day_24.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Author: Joshua Ampofo Yentumi
+
+Challenge Day 24: Average Calories
+
+Description: Implement a function called `average calories` that calculates the average
+             calorie intake of a user. The function should ask the user to imput their
+             calorie intake for "any number of days", and once they hit "done", it should calculate and return the average intake.
+"""
+def average_calories() -> int:
+    """Calculates the average calorie of a user"""
+    
+    calories_list = []
+    while True:
+        try:
+            calorie = input("Calories (input done to end program): ")
+            if calorie == "done":
+                break
+            calories_list.append(int(calorie))
+        except ValueError:
+            return 'Input Invalid. Try again'
+    return f"Average calories intake: {sum(calories_list)/len(calories_list):.2f}"
+
+
+if __name__ == "__main__":
+    print(average_calories())
